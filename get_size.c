a17da425af8c62e2cf6446ca47af14746021fd16
Hensha-w <himtwo024@gmail.com>
2023-09-22 10:17:13 -0700

    side

--- get_size.c
+++ get_size.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_size - calculates the size to cast the arguments
+ * @format: formatted string to print the arguments
+ * @i: list of arguments to be printed.
  *
- * Return: Precision.
+ * Return: precision.
  */
-int get_size(const char *format, int *i)
+int get_size(const char *format, int *j)
 {
-	int curr_i = *i + 1;
+	int curr_j = *j + 1;
 	int size = 0;
 
-	if (format[curr_i] == 'l')
+	if (format[curr_j] == 'l')
 		size = S_LONG;
-	else if (format[curr_i] == 'h')
+	else if (format[curr_j] == 'h')
 		size = S_SHORT;
 
 	if (size == 0)
-		*i = curr_i - 1;
+		*j = curr_j - 1;
 	else
-		*i = curr_i;
+		*j = curr_j;
 
 	return (size);
 }
--- handle_print.c
+++ handle_print.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
+ * handle_print - prints an argument based on its type
+ * @fmt: formatted string to print the arguments.
+ * @list: list of arguments to be printed.
  * @ind: ind.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
+ * @buffer: buffer to handle print.
+ * @flags: calculates the active flags
  * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
+ * @precision: precision specification
+ * @size: size specifier
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow_len = 0, printed_chars = -1;
+	int j, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -22,11 +22,11 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+	for j = 0; fmt_types[j].fmt != '\0'; j++)
+		if (fmt[*ind] == fmt_types[j].fmt)
+			return (fmt_types[j].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[i].fmt == '\0')
+	if (fmt_types[j].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
